9947567bfa49b2914ab91a0ea496654d7dfd9061
shed <121990815+ShadeeSkiee@users.noreply.github.com>
2026-07-23 23:16:30 +0700

    Add_phone_field_to_AuthController_register_validation

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -50,6 +50,7 @@ class AuthController extends Controller
             'name' => 'required|string|max:255',
             'username' => 'required|string|max:255|unique:users',
             'email' => 'required|string|email|max:255|unique:users',
+            'phone' => 'nullable|string|max:255',
             'password' => 'required|string|min:6',
         ]);
 
